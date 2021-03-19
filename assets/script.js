f23bb043e26ad48ff36a3e59136a66775be8839d
b729 <mowrey729@gmail.com>
2021-03-19 10:19:47 -0500

    Current Weather data added

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,11 @@
-$(document).ready(function(){
-  var searchHistoryContainer =$('#past-searches');
+$(document).ready(function () {
+  var searchHistoryContainer = $('#past-searches');
   var searchForm = $('#search-form');
+  var currentWeatherContainer = $('#current-weather');
+  var fiveDayForcast
   var apiKey = 'cd0b3fe1a800e29bb189f5148cda1151'
-  var baseUrl = '';
-  searchForm.submit(function(event){    
+  var baseUrl = 'https://api.openweathermap.org/data/2.5/weather?';
+  searchForm.submit(function (event) {
     event.preventDefault();
     console.log(event);
     var formValues = $(this).serializeArray();
@@ -12,21 +14,44 @@ $(document).ready(function(){
     searchTermDiv.text(city);
     searchHistoryContainer.append(searchTermDiv);
     console.log(formValues, city);
-    searchForCityWeather(city);
+    searchForCurrentCityWeather(city);
   });
-  function searchForCityWeather(city){
-    console.log(city);
+  function searchForCurrentCityWeather(city) {
+    var fullUrl = baseUrl + "q=" + city + "&appid=" + apiKey;
+    console.log(fullUrl);
+    fetch(fullUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+        var cityName = data.name;
+        var temp = data.main.temp;
+        var humidity = data.main.humidity;
+        var weather = data.weather;
+        var wind = data.wind
+        var cityNameDiv = $("<div class='city-name'>")
+        var tempDiv = $("<div class='temp-name'>")
+        var humidityDiv = $("<div class='humidity-name'>")
+        var weatherDiv = $("<div class='icon-name'>")
+        var windDiv = $("<div class='wind-name'>")
+        cityNameDiv.text(cityName);
+        tempDiv.text('Temperature:  ' + temp);
+        humidityDiv.text('Humidity:  ' + humidity + '%');
+        windDiv.text('Wend Speed: ' + wind.speed + ' MPH')
+
+        currentWeatherContainer.append(cityNameDiv);
+        currentWeatherContainer.append(tempDiv);
+        currentWeatherContainer.append(humidityDiv);
+
+        currentWeatherContainer.append(windDiv);
+      });
   }
+  function searchForFiveDayForcastWeather(city) {
+
+  }
+
 });
 
 
 
-//fetch('https://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=cd0b3fe1a800e29bb189f5148cda1151', {
-  
-
-  //.then(function (response) {
-    //return response.json();
-  //})
-  //.then(function (data) {
-   // console.log(data);
-  //});
